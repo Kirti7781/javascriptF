2ad2c274ec4dbbd2914f0a2e2a752ce3d0cdef39
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-03 08:39:00 +0000

    comparisions done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+
+// avoid such comparsions
+console.log(null >0)
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined >0)
+console.log(undefined == 0);
+console.log(undefined <0);
+
+
+
+
+// comparision and equality checks work differently and hence the answer
+
+// === strict check (compares value as well as datatypes)
