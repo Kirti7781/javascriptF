28587acaf53211ae30b337e11ffcc0814f97201d
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-03 07:42:11 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) since we are using node js and not browser
+console.log("kirt")
+console.log(3+3)
+
+// numbers => 2 to the power 53
+// bigint
+// string =>within double quotes
+// bool =>true/false
+//null =>standalone value
+// undefined=>value yet not defined
+// symbol =>unique
+
+//object
+console.log(typeof "kirti")
+console.log(typeof undefined) //type is undefined
+console.log(typeof null) //type is object
+
