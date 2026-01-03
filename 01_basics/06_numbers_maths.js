a9df6c47fb821871db3668bf6fdfa31ab55ecfe7
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-03 18:01:08 +0000

    numbers nd math done

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,31 @@
+const num=400
+const numb=new Number(500)
+console.log(num)
+console.log(numb)
+
+const nu=100
+console.log(nu.toFixed(2))
+const n=123.4567
+console.log(n.toPrecision(2))
+const hund=1000000
+console.log(hund.toLocaleString("en-IN"))
+console.log(hund.toLocaleString())
+
+//++++++++++++++++++++++++++++++++MATH++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.39));
+console.log(Math.ceil(4.39));
+console.log(Math.floor(4.39));
+console.log(Math.max(4,9,1,6,3));
+console.log(Math.min(4,9,1,6,3));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
