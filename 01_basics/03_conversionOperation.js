8e10f5ec4fbb21b1f8381b61543c60a3efd0fd6d
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-03 08:04:45 +0000

    conversions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33abs"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInnumber=Number(score);
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+
+//"33" => 33
+//"33abc" =>NaN
+// null=> 0
+//undefined => NaN
+// true =>1 ; false =>
+
+let isLOggedin=" "
+let isboolloggedin=Boolean(isLOggedin)
+console.log(isboolloggedin)
+
+// 1=>true
+// 0=>false
+// ""=>false
+// "kirti"=>true
