2202c3ebcc6103b8ab5dff47fadd8996097e68ce
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-03 07:19:08 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId=14356
+let accountEmail="kirti@gmail.com"
+var accountpassword=2345
+accountcity="jaipur"
+let accountState;
+
+// accountId=2 not allowed//
+/*use of var should be avoided*/
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
