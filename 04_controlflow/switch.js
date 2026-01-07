9a0778f76af7e004226af1c8580ae2e7e8bb3980
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-07 14:46:45 +0000

    switch done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month=3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    default:
+        console.log("none");
+        
+        break;
+}
