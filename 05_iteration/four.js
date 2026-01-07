c62d889a89b4a0f4ef7db8a4e59956fa936dd090
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-07 17:01:53 +0000

    for in loop done

---
+++ 05_iteration/four.js
@@ -0,0 +1,23 @@
+// forin loop
+
+const myobj={
+    name:'krtik',
+    age:21,
+    role:'developer'
+}
+for (const key in myobj) {
+    console.log(`the ${key}  has value  ${myobj[key]}`);
+}
+
+// const mymap=new Map();
+// mymap.set('name','krtik');
+// mymap.set('age',21);
+// mymap.set('role','developer');
+// mymap.set('name','krtik');
+
+// for (const key in mymap) {
+//     console.log(`the ${key}  has value  ${mymap[key]}`); //doesnt work for maps
+    
+    
+    
+// } doesnt work for maps
