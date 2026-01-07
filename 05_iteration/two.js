08d447a38ed631d335024056713b13daa75f511e
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-07 16:15:26 +0000

    while do while done

---
+++ 05_iteration/two.js
@@ -0,0 +1,11 @@
+let i=1
+while (i<=10) {
+    console.log(`vale of i ${i}`);
+    i=i*2
+    
+}
+let score=11
+do {
+    console.log(`vale of score is ${score}`);
+    score++
+} while (score<=10);
