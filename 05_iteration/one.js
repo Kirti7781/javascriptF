64cfdad28eab0a0aab48caa18befbcbfd86b7718
Kirti Singh <kirtisinghchauhan7781@gmail.com>
2026-01-07 16:05:01 +0000

    for loop done

---
+++ 05_iteration/one.js
@@ -0,0 +1,13 @@
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+const myarr=['krtik','anurag','rahul'];
+for (let i = 0; i < myarr.length; i++) {
+    console.log(myarr[i]);
+    
+}
+//break continue
